708fa9cc92c6f4b1cdf52d4422e472c55cb81b72
Bstar098 <luceropintos@abc.gob.ar>
2022-12-19 18:10:57 -0300

    initial configurations done

---
+++ screens/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Test({navigation}) {
+  return (
+    <View>
+      <Text>Testing this challenge out uwu</Text>
+    </View>
+  );
+}
+
+export default Test;
